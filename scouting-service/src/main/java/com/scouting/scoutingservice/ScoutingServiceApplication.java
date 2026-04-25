d8fb675062a5f094b5e4fc63898ac754b915dfdb
Talha Fırat <tfmesebusiness@gmail.com>
2026-04-25 16:39:21 +0300

    servis iskeletleri ve gateway eklendi.

---
+++ scouting-service/src/main/java/com/scouting/scoutingservice/ScoutingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.scouting.scoutingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoutingServiceApplication.class, args);
+    }
+
+}
---
+++ scouting-service/src/test/java/com/scouting/scoutingservice/ScoutingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scouting.scoutingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
